323e939e599382a1e7c7332bb1c9d4b77ff06e64
Jyoti Dhatarwal <65608438+jyotidhatarwal@users.noreply.github.com>
2021-03-02 14:27:27 +0530

    15 ms

--- solutions/645. Set Mismatch.java
+++ solutions/645. Set Mismatch.java
@@ -1,13 +1,17 @@
 public class Solution {
     public int[] findErrorNums(int[] nums) {
-        Arrays.sort(nums);
+        Map < Integer, Integer > map = new HashMap();
         int dup = -1, missing = 1;
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] == nums[i - 1])
-                dup = nums[i];
-            else if (nums[i] > nums[i - 1] + 1)
-                missing = nums[i - 1] + 1;
+        for (int n: nums) {
+            map.put(n, map.getOrDefault(n, 0) + 1);
         }
-        return new int[] {dup, nums[nums.length - 1] != nums.length ? nums.length : missing};
+        for (int i = 1; i <= nums.length; i++) {
+            if (map.containsKey(i)) {
+                if (map.get(i) == 2)
+                    dup = i;
+            } else
+                missing = i;
+        }
+        return new int[]{dup, missing};
     }
 }
