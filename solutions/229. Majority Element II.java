08234a88dd2b17f3352915917b1c3889cfd5eccc
Jyoti Dhatarwal <65608438+jyotidhatarwal@users.noreply.github.com>
2021-03-06 11:30:21 +0530

    2 ms

--- solutions/229. Majority Element II.java
+++ solutions/229. Majority Element II.java
@@ -1,16 +1,46 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        HashMap<Integer,Integer> map = new HashMap<>();
+        int count1= 0;
+        int count2 =0;
+        int num1 =-1;
+        int num2 =-1;
         List<Integer> res = new ArrayList<>();
-        for(int i=0;i<nums.length;i++){
-            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
-        }
         int majority = (int)Math.floor(nums.length/3);
-        for(int val:map.keySet()){
-            if(map.get(val) > majority){
-                res.add(val);
+​
+        for(int element : nums){
+            if(element == num1){
+                count1++;
+            }else if(element == num2){
+                count2++;
+            }else if(count1 ==0){
+                num1 = element;
+                count1 =1;
+            }else if(count2 ==0){
+                num2 = element;
+                count2=1;
+            }else{
+                count1--;
+                count2--;
             }
         }
+        
+         count1 =0;
+         count2=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i] == num1){
+                count1++;
+            }else if(nums[i] == num2){
+                count2++;
+            }
+            
+        }
+        if(count1 > majority){
+            res.add(num1);
+        }
+        if(count2 > majority){
+            res.add(num2);
+        }
+       
         return res;
     }
 }
