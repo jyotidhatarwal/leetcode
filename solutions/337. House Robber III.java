33c531e979e3f0a2404b2bbea719b5216c597401
Jyoti Dhatarwal <65608438+jyotidhatarwal@users.noreply.github.com>
2021-03-31 09:40:25 +0530

    0 ms

--- solutions/337. House Robber III.java
+++ solutions/337. House Robber III.java
@@ -14,24 +14,39 @@
  * }
  */
 class Solution {
-    class HousePair{
-        int withRobbery =0;
-        int withoutRobbery =0;
-    }
-    public HousePair robHelp(TreeNode root){
-        if(root == null) return new HousePair();
+    // class HousePair{
+    //     int withRobbery =0;
+    //     int withoutRobbery =0;
+    // }
+//     public HousePair robHelp(TreeNode root){
+//         if(root == null) return new HousePair();
         
-        HousePair left = robHelp(root.left);
-        HousePair right = robHelp(root.right);
-        HousePair ans = new HousePair();
+//         HousePair left = robHelp(root.left);
+//         HousePair right = robHelp(root.right);
+//         HousePair ans = new HousePair();
         
-        ans.withRobbery = left.withoutRobbery + root.val + right.withoutRobbery;
-        ans.withoutRobbery = Math.max(left.withRobbery,left.withoutRobbery) + Math.max(right.withRobbery,right.withoutRobbery);
+//         ans.withRobbery = left.withoutRobbery + root.val + right.withoutRobbery;
+//         ans.withoutRobbery = Math.max(left.withRobbery,left.withoutRobbery) + Math.max(right.withRobbery,right.withoutRobbery);
+//         return ans;
+        
+//     }
+   // public int rob(TreeNode root) {
+    //     HousePair result = robHelp(root);
+    //     return Math.max(result.withRobbery,result.withoutRobbery);
+    // }
+    /*   Array --> with robbery , without robbery   */
+    
+    public int[] robHelp(TreeNode root){
+        if(root == null) return new int[2];
+        int[] left = robHelp(root.left);
+        int[] right = robHelp(root.right);
+        int[] ans = new int[2];
+        ans[0] = left[1] + root.val + right[1];
+        ans[1] = Math.max(left[0],left[1]) + Math.max(right[0],right[1]);
         return ans;
-        
     }
-    public int rob(TreeNode root) {
-        HousePair result = robHelp(root);
-        return Math.max(result.withRobbery,result.withoutRobbery);
+    public int rob(TreeNode root){
+        int[] result = robHelp(root);
+        return Math.max(result[0],result[1]);
     }
 }
