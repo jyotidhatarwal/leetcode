542a30f0f0f9226eb3834c497833ed993f7039e7
Jyoti Dhatarwal <65608438+jyotidhatarwal@users.noreply.github.com>
2021-04-03 12:10:27 +0530

    0 ms

---
+++ solutions/94. Binary Tree Inorder Traversal.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private void inorderHelp(TreeNode root,List<Integer> ans){
+        if(root == null) return;
+        inorderHelp(root.left,ans);
+        ans.add(root.val);
+        inorderHelp(root.right,ans);
+    }
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        inorderHelp(root,ans);
+        return ans;
+    }
+}
