dded441798cbd44c4627f06c84cc4eaf165e5fed
Jyoti Dhatarwal <65608438+jyotidhatarwal@users.noreply.github.com>
2021-04-05 11:42:26 +0530

    6 ms

--- solutions/234. Palindrome Linked List.java
+++ solutions/234. Palindrome Linked List.java
@@ -10,9 +10,7 @@
  */
 class Solution {
     private ListNode midOfLL(ListNode head){
-        if(head == null || head.next == null){
-            return head;
-        }
+        if(head == null || head.next == null) return head;
         ListNode slow = head;
         ListNode fast = head;
         while(fast.next != null && fast.next.next != null){
@@ -22,11 +20,9 @@ class Solution {
         return slow;
     }
     private ListNode reverse(ListNode head){
-        if(head == null || head.next == null){
-            return head;
-        }
+        if(head == null || head.next == null) return head;
         ListNode prev = null;
-        ListNode curr =head;
+        ListNode curr = head;
         while(curr != null){
             ListNode next = curr.next;
             curr.next = prev;
@@ -35,29 +31,25 @@ class Solution {
         }
         return prev;
     }
-    
     public boolean isPalindrome(ListNode head) {
-        if(head == null || head.next ==null){
-            return true;
-        }
+        if(head == null || head.next == null) return true;
         ListNode mid = midOfLL(head);
         ListNode newHead = mid.next;
         mid.next = null;
         newHead = reverse(newHead);
-        ListNode current1 = head;
-        ListNode current2 = newHead;
+        ListNode c1 = head;
+        ListNode c2 = newHead;
         boolean result = true;
-        while(current2 != null){
-            if(current1.val != current2.val){
+        while(c2 != null){
+            if(c1.val != c2.val){
                 result = false;
                 break;
             }
-            current1 = current1.next;
-            current2 = current2.next;
+            c1 = c1.next;
+            c2 = c2.next;
         }
         newHead = reverse(newHead);
         mid.next = newHead;
         return result;
-        
     }
 }
