197d9522bd8897ccde2dc013039bfc285069c0f5
Jyoti Dhatarwal <65608438+jyotidhatarwal@users.noreply.github.com>
2021-04-05 11:33:57 +0530

    0 ms

--- solutions/141. Linked List Cycle.java
+++ solutions/141. Linked List Cycle.java
@@ -10,21 +10,17 @@
  * }
  */
 public class Solution {
-    public boolean hasCycle(ListNode head) {       
-        if(head == null){
-            return false;
-        }
+    public boolean hasCycle(ListNode head) {
+        if(head == null) return false;
+        
         ListNode slow = head;
         ListNode fast = head.next;
         while(slow != fast){
-            if(fast==null || fast.next ==null){
-                return false;
-            }
+            if(fast == null || fast.next == null) return false;
             slow = slow.next;
             fast = fast.next.next;
         }
         return true;
+        
     }
 }
-/*    The time complexity of this is O(n)  */
-/*    The space complexity of this is O(1)  */
