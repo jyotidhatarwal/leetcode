6b3a7f469aeaa0f3f5de1d2c41960809404aad9c
Jyoti Dhatarwal <65608438+jyotidhatarwal@users.noreply.github.com>
2021-04-10 16:20:12 +0530

    0 ms

--- solutions/53. Maximum Subarray.java
+++ solutions/53. Maximum Subarray.java
@@ -1,19 +1,17 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-       int sum =0;
-        int maximum = nums[0];
-        for(int i=0;i<nums.length;i++){
-            sum += nums[i];
-            if(sum > maximum){
-                maximum = sum;
+        int currentSum = nums[0];
+        int overallSum = nums[0];
+        for(int i=1;i<nums.length;i++){
+            if(currentSum >0){
+                currentSum += nums[i];
+            }else{
+                currentSum = nums[i];
             }
-            if(sum <0){
-                sum =0;
+            if(currentSum > overallSum){
+                overallSum = currentSum;
             }
         }
-        return maximum;
+        return overallSum;
     }
 }
-/*  This is done by using kadane's Algorithm which states that if the current sum is positive then add itself to the current sum and if it is negative then start fresh make current sum equal to itself and at last return the overall best sum    */
-​
-/*   the time complexity of this code is O(n) and the space complexit of this is O(1)       */
