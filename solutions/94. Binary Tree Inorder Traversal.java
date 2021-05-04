147e26e16e498bcb1278516f10f6d2d9a83867d6
Jyoti Dhatarwal <65608438+jyotidhatarwal@users.noreply.github.com>
2021-05-04 10:04:43 +0530

    0 ms

--- solutions/94. Binary Tree Inorder Traversal.java
+++ solutions/94. Binary Tree Inorder Traversal.java
@@ -14,15 +14,27 @@
  * }
  */
 class Solution {
-    private void inorderHelp(TreeNode root,List<Integer> ans){
-        if(root == null) return;
-        inorderHelp(root.left,ans);
-        ans.add(root.val);
-        inorderHelp(root.right,ans);
-    }
     public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> ans = new ArrayList<>();
-        inorderHelp(root,ans);
+      List<Integer> ans = new ArrayList<>();
+        while(root != null){
+            if(root.left == null){
+                ans.add(root.val);
+                root = root.right;
+            }else{
+                TreeNode rootp1 = root.left;
+                while(rootp1.right != null && rootp1.right != root){
+                    rootp1 = rootp1.right;
+                }
+                if(rootp1.right == null){
+                    rootp1.right = root;
+                    root = root.left;
+                }else{
+                    ans.add(root.val);
+                    rootp1.right = null;
+                    root = root.right;
+                }
+            }
+        }
         return ans;
     }
 }
