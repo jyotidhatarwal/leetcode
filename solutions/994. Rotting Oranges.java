2b785a6c8a4e946a45c43cd26692546150e95619
Jyoti Dhatarwal <65608438+jyotidhatarwal@users.noreply.github.com>
2021-05-24 09:48:29 +0530

    2 ms

--- solutions/994. Rotting Oranges.java
+++ solutions/994. Rotting Oranges.java
@@ -2,14 +2,13 @@ class Solution {
     public int orangesRotting(int[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
-        if(n== 0 && m == 0) return 0;
-        LinkedList<int[]> que = new LinkedList<>();
         int fresh_orange =0;
+        LinkedList<int[]> que = new LinkedList<>();
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j] == 1){
                     fresh_orange++;
-                }else if(grid[i][j] == 2){  // adding position of rotten orange in que
+                }else if(grid[i][j] == 2){
                     que.add(new int[]{i,j});
                 }
             }
@@ -22,29 +21,5 @@ class Solution {
                 int[] rem = que.removeFirst();
                 int x = rem[0];
                 int y = rem[1];
-                if(x-1>=0 && grid[x-1][y] == 1){
-                    grid[x-1][y] = 2;
-                    que.add(new int[]{x-1,y});
-                    fresh_orange--;
-                }
-                if(y-1>=0 && grid[x][y-1] == 1){
-                    grid[x][y-1] = 2;
-                    que.add(new int[]{x,y-1});
-                    fresh_orange--;
-                }
-                if(x+1<grid.length && grid[x+1][y] == 1){
-                    grid[x+1][y] = 2;
-                    que.add(new int[]{x+1,y});
-                    fresh_orange--;
-                }
-                if(y+1<grid[0].length && grid[x][y+1] == 1){
-                    grid[x][y+1] = 2;
-                    que.add(new int[]{x,y+1});
-                    fresh_orange--;
-                }
-            }
-            minute++;
-        }
-        return fresh_orange == 0 ? minute-1 : -1;
-    }
-}
+                if(x-1 >=0 && grid[x-1][y] == 1){
+                    grid[x-1][y] =2;
