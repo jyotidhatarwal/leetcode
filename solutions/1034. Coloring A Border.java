3985a1c459f1fd637d242cf1db69c764dc080e9d
Jyoti Dhatarwal <65608438+jyotidhatarwal@users.noreply.github.com>
2021-05-24 09:41:58 +0530

    0 ms

--- solutions/1034. Coloring A Border.java
+++ solutions/1034. Coloring A Border.java
@@ -3,28 +3,25 @@ class Solution {
         int n = grid.length;
         int m = grid[0].length;
         boolean[][] visited = new boolean[n][m];
-        if(grid[r0][c0] == color){
-            return grid;
-        }
-        
+        if(n == 0 && m== 0) return grid;
+        if(grid[r0][c0] == color) return grid;
         dfs(grid,r0,c0,grid[r0][c0],visited,color,n,m);
         return grid;
     }
-    private void dfs(int[][]grid,int row,int col,int val,boolean[][]visited,int color,int n,int m){
-        if(row > n-1 || row <0 ||  col > m-1 || col <0 || grid[row][col] != val || visited[row][col]){
+    private void dfs(int[][] grid,int row,int col,int val,boolean[][]visited,int color,int n,int m){
+        if(row <0 || row > n-1 || col <0 || col > m-1 || grid[row][col] != val || visited[row][col]){
             return;
         }
         visited[row][col] = true;
         boolean border = false;
-        if(row==0 || col== 0 ||row == n-1 || col == m-1 || grid[row+1][col] != val || grid[row-1][col] != val || grid[row][col +1 ] != val || grid[row][col-1] != val ){
+        if(row == 0 || row == n-1 || col == 0 || col == m-1 || grid[row+1][col] != val || grid[row-1][col] != val || grid[row][col+1] != val || grid[row][col-1] != val){
             border = true;
         }
         
-        dfs(grid,row+1,col,val,visited,color,n,m);  // top
-        dfs(grid,row-1,col,val,visited,color,n,m);  // bottom
-        dfs(grid,row,col+1,val,visited,color,n,m);  // right
-        dfs(grid,row,col-1,val,visited,color,n,m);  // left
-        
+        dfs(grid,row+1,col,val,visited,color,n,m);
+         dfs(grid,row-1,col,val,visited,color,n,m);
+         dfs(grid,row,col+1,val,visited,color,n,m);
+         dfs(grid,row,col-1,val,visited,color,n,m);
         if(border){
             grid[row][col] = color;
         }
