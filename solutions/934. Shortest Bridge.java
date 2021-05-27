8c4918cfe727044549d17978cfbf346a06bfe6be
Jyoti Dhatarwal <65608438+jyotidhatarwal@users.noreply.github.com>
2021-05-27 07:41:08 +0530

    7 ms

--- solutions/934. Shortest Bridge.java
+++ solutions/934. Shortest Bridge.java
@@ -1,53 +1,59 @@
 class Solution {
-    public int shortestBridge(int[][] A) {
-        int m = A.length, n = A[0].length;
+    
+    int[][] dirs = {{1,0},{0,1},{-1,0},{0,-1}};
+    
+    public int shortestBridge(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
         boolean[][] visited = new boolean[m][n];
-        int[][] dirs = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-        Queue<int[]> q = new LinkedList<>();
-        boolean found = false;
-        // 1. dfs to find an island, mark it in `visited`
-        for (int i = 0; i < m; i++) {
-            if (found) {
+        LinkedList<int[]> que = new LinkedList<>();
+        boolean found = false;  // found 1
+        // step-1 marking visited for all 1's
+        for(int i=0;i<m;i++){
+            if(found == true){
                 break;
             }
-            for (int j = 0; j < n; j++) {
-                if (A[i][j] == 1) {
-                    dfs(A, visited, q, i, j, dirs);
+            for(int j=0;j<n;j++){
+                if(grid[i][j] == 1){
+                    dfs(grid,visited,i,j,que,dirs);
                     found = true;
                     break;
                 }
             }
         }
-        // 2. bfs to expand this island
-        int step = 0;
-        while (!q.isEmpty()) {
-            int size = q.size();
-            while (size-- > 0) {
-                int[] cur = q.poll();
-                for (int[] dir : dirs) {
-                    int i = cur[0] + dir[0];
-                    int j = cur[1] + dir[1];
-                    if (i >= 0 && j >= 0 && i < m && j < n && !visited[i][j]) {
-                        if (A[i][j] == 1) {
-                            return step;
-                        }
-                        q.offer(new int[]{i, j});
-                        visited[i][j] = true;
+        // step-2 calculating flips
+        int step =0;
+        while(que.size() > 0){
+            int size = que.size();
+            while(size-- > 0){
+                int[] curr = que.remove();
+               
+                for(int[] dir : dirs){
+                     int x = curr[0] + dir[0];
+                     int y = curr[1] + dir[1];
+                    if(x >=0 && y>=0 && x < grid.length && y < grid[0].length && visited[x][y] == false){
+                     if(grid[x][y] == 1){
+                         return step;
+                     }
+                        que.add(new int[]{x,y});
+                        visited[x][y] = true;
                     }
+                   
                 }
             }
             step++;
         }
         return -1;
+        
     }
-    private void dfs(int[][] A, boolean[][] visited, Queue<int[]> q, int i, int j, int[][] dirs) {
-        if (i < 0 || j < 0 || i >= A.length || j >= A[0].length || visited[i][j] || A[i][j] == 0) {
+    private void dfs(int[][] grid,boolean[][] visited,int i, int j, LinkedList<int[]> que,int[][] dirs){
+        if(i < 0 || j <0 || i >= grid.length || j >= grid[0].length || visited[i][j] == true || grid[i][j] == 0){
             return;
         }
         visited[i][j] = true;
-        q.offer(new int[]{i, j});
-        for (int[] dir : dirs) {
-            dfs(A, visited, q, i + dir[0], j + dir[1], dirs);
+        que.add(new int[]{i,j});
+        
+        for(int[] dir : dirs){
+            dfs(grid,visited,i+dir[0],j+dir[1],que,dirs);
         }
     }
-}
