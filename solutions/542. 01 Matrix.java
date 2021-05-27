22728f2cdd319760ccde02b5fe12f3569effcb9a
Jyoti Dhatarwal <65608438+jyotidhatarwal@users.noreply.github.com>
2021-05-27 07:50:46 +0530

    12 ms

--- solutions/542. 01 Matrix.java
+++ solutions/542. 01 Matrix.java
@@ -1,34 +1,34 @@
-public class Solution {
-    public int[][] updateMatrix(int[][] matrix) {
-        int m = matrix.length;
-        int n = matrix[0].length;
+class Solution {
+    int[][] dirs = {{1,0},{0,1},{-1,0},{0,-1}};
+    public int[][] updateMatrix(int[][] mat) {
+        int m = mat.length;
+        int n = mat[0].length;
+        LinkedList<int[]> que = new LinkedList<>();
         
-        Queue<int[]> queue = new LinkedList<>();
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (matrix[i][j] == 0) {
-                    queue.offer(new int[] {i, j});
-                }
-                else {
-                    matrix[i][j] = Integer.MAX_VALUE;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(mat[i][j] == 0){
+                    que.add(new int[]{i,j});
+                }else {
+                    mat[i][j] = Integer.MAX_VALUE;
                 }
             }
         }
         
-        int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-        
-        while (!queue.isEmpty()) {
-            int[] cell = queue.poll();
-            for (int[] d : dirs) {
-                int r = cell[0] + d[0];
-                int c = cell[1] + d[1];
-                if (r < 0 || r >= m || c < 0 || c >= n || 
-                    matrix[r][c] <= matrix[cell[0]][cell[1]] + 1) continue;
-                queue.add(new int[] {r, c});
-                matrix[r][c] = matrix[cell[0]][cell[1]] + 1;
-            }
+        while(que.size() > 0){
+            int size = que.size();
+           
+                int[] curr = que.remove();
+                
+                for(int[] dir : dirs){
+                    int x = curr[0] + dir[0];
+                    int y = curr[1] + dir[1];
+                    if(x < 0 || x >= mat.length || y < 0|| y >= mat[0].length || mat[x][y] <= mat[curr[0]][curr[1]] +1) continue;
+                    que.add(new int[]{x,y});
+                    mat[x][y] = mat[curr[0]][curr[1]] +1;
+                }
+            
         }
-        
-        return matrix;
+        return mat;
     }
 }
