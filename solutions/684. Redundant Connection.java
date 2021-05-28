e9951de68ec747a430a6a0e4174e20dae16b5653
Jyoti Dhatarwal <65608438+jyotidhatarwal@users.noreply.github.com>
2021-05-28 08:01:09 +0530

    0 ms

--- solutions/684. Redundant Connection.java
+++ solutions/684. Redundant Connection.java
@@ -2,25 +2,28 @@ class Solution {
     // using union find
     int[] par;
    private int findPar(int u){
-       if (u == par[u])
+       if (par[u] == u)
           return u;
-       return par[u] = findPar(par[u]);
+       int temp = findPar(par[u]);
+       par[u] = temp;
+       return temp;
     }
     public int[] findRedundantConnection(int[][] edges) {
          int n = edges.length;
          par = new int[n+1];
+        // initially everyone is parent of itself only
          for (int i = 0; i <= n; i++){
              par[i] = i;
          }
 ​
-            for (int[] a : edges){
-             int p1 = findPar(a[0]);
-            int p2 = findPar(a[1]);
+            for (int[] ans : edges){
+            int p1 = findPar(ans[0]);
+            int p2 = findPar(ans[1]);
 ​
                 if (p1 != p2){
                     par[p1] = p2;
                  }else{
-                    return a;
+                    return ans;
              }
             }
 ​
