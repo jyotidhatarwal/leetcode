939bb82a5eec89deed07df2929ca26a1118a3896
Jyoti Dhatarwal <65608438+jyotidhatarwal@users.noreply.github.com>
2021-05-28 09:29:58 +0530

    10 ms

--- solutions/839. Similar String Groups.java
+++ solutions/839. Similar String Groups.java
@@ -11,7 +11,12 @@ class Solution {
     private boolean isSimilar(String s1,String s2){
         int count =0;
         for(int i=0;i<s1.length();i++){
-            if(s1.charAt(i) != s2.charAt(i) && ++count > 2 ) return false;
+            if(s1.charAt(i) != s2.charAt(i)){
+                count++;
+                if(count > 2){
+                    return false;
+                }
+            }
         }
         return true;
     }
