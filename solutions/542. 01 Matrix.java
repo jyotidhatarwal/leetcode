822c2a14b76014f4375e4f0a4386f21dd18f2c87
Jyoti Dhatarwal <65608438+jyotidhatarwal@users.noreply.github.com>
2021-06-01 12:49:27 +0530

    20 ms

--- solutions/542. 01 Matrix.java
+++ solutions/542. 01 Matrix.java
@@ -1,15 +1,17 @@
 class Solution {
-    int[][] dirs = {{1,0},{0,1},{-1,0},{0,-1}};
+    int[][] dirs = {{-1,0},{0,-1},{0,1},{1,0}};
+    
     public int[][] updateMatrix(int[][] mat) {
-        int m = mat.length;
-        int n = mat[0].length;
-        LinkedList<int[]> que = new LinkedList<>();
+        int n = mat.length;
+        int m = mat[0].length;
         
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
+       LinkedList<int[]> que = new LinkedList<>();
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
                 if(mat[i][j] == 0){
                     que.add(new int[]{i,j});
-                }else {
+                }else{
                     mat[i][j] = Integer.MAX_VALUE;
                 }
             }
@@ -17,15 +19,17 @@ class Solution {
         
         while(que.size() > 0){
             int size = que.size();
-           
-                int[] curr = que.remove();
-                
+            
+                int[] rem = que.removeFirst();
+                int i = rem[0];
+                int j = rem[1];
                 for(int[] dir : dirs){
-                    int x = curr[0] + dir[0];
-                    int y = curr[1] + dir[1];
-                    if(x < 0 || x >= mat.length || y < 0|| y >= mat[0].length || mat[x][y] <= mat[curr[0]][curr[1]] +1) continue;
+                    int x = i + dir[0];
+                    int y = j + dir[1];
+                    if(x < 0 || y < 0 || x >= mat.length || y >= mat[0].length || mat[x][y] <= mat[i][j]+1) continue;
                     que.add(new int[]{x,y});
-                    mat[x][y] = mat[curr[0]][curr[1]] +1;
+                    mat[x][y] = mat[i][j] +1;
+                    
                 }
             
         }
