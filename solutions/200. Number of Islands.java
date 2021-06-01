28a1c27b9443102c89abd9a64420815cf0a14184
Jyoti Dhatarwal <65608438+jyotidhatarwal@users.noreply.github.com>
2021-06-01 12:36:54 +0530

    1 ms

--- solutions/200. Number of Islands.java
+++ solutions/200. Number of Islands.java
@@ -1,33 +1,29 @@
 class Solution {
+    
     public int numIslands(char[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-        boolean[][] visited = new boolean[m][n];
-        int ans =0;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
+     int n = grid.length;
+        int m = grid[0].length;
+        boolean[][] visited = new boolean[n][m];
+       int count=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
                 if(grid[i][j] == '1' && visited[i][j] == false){
-              dfs(grid,visited,i,j,m,n);
-                ans++;
-             }
+                    dfs(grid,i,j,n,m,visited);
+                    count++;
                 }
             }
-         return ans;
         }
-       
-    
-    private void dfs(char[][]grid,boolean[][] visited,int i,int j,int m,int n){
-        
-        if(i < 0 || i >= m || j <0 || j>= n || grid[i][j] == '0' || visited[i][j] == true){
-            return ;
+        return count;
+    }
+    private void dfs(char[][] grid,int i,int j,int n,int m,boolean[][] visited){
+        if(i <0 || j<0 || i>= n || j>=m || grid[i][j] == '0' || visited[i][j] == true){
+            return;
         }
         visited[i][j] = true;
         
-        dfs(grid,visited,i+1,j,m,n);
-        dfs(grid,visited,i-1,j,m,n);
-        dfs(grid,visited,i,j+1,m,n);
-        dfs(grid,visited,i,j-1,m,n);
-        
-        
+        dfs(grid,i-1,j,n,m,visited);
+        dfs(grid,i,j-1,n,m,visited);
+        dfs(grid,i,j+1,n,m,visited);
+        dfs(grid,i+1,j,n,m,visited);
     }
 }
