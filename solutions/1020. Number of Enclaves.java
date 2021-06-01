c2d10da921b7977c6bd1b3b8a75e8bfa3464031b
Jyoti Dhatarwal <65608438+jyotidhatarwal@users.noreply.github.com>
2021-06-01 12:29:53 +0530

    7 ms

--- solutions/1020. Number of Enclaves.java
+++ solutions/1020. Number of Enclaves.java
@@ -1,36 +1,34 @@
 class Solution {
     public int numEnclaves(int[][] grid) {
-        int ans =0;
-        int m = grid.length;
-        int n = grid[0].length;
-        
+        int n = grid.length;
+        int m = grid[0].length;
         // travel on boundary and change 1 to 0
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(i == 0 || j== 0 || i== m-1 || j == n-1){
-                    change1to0(grid,i,j,m,n);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i==0 || j== 0 || i == n-1 || j== m-1){
+                    dfs(grid,i,j,n,m);
                 }
             }
         }
-        
-        // calculate the 1's which are not connected to boundary
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
+        int ans =0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
                 if(grid[i][j] == 1){
                     ans++;
                 }
             }
         }
         return ans;
+        
     }
-    private void change1to0(int[][]grid,int i,int j,int m,int n){
-        if(i <0 || j <0 || i>= m || j >=n || grid[i][j] == 0){
+    private void dfs(int[][] grid,int i,int j,int n,int m){
+        if(i < 0 || j <0 || i>= n || j >= m || grid[i][j] == 0){
             return;
         }
-        grid[i][j] = 0;
-        change1to0(grid,i+1,j,m,n);
-        change1to0(grid,i-1,j,m,n);
-        change1to0(grid,i,j+1,m,n);
-        change1to0(grid,i,j-1,m,n);
+        grid[i][j] =0;
+        dfs(grid,i-1,j,n,m);
+        dfs(grid,i,j-1,n,m);
+        dfs(grid,i,j+1,n,m);
+        dfs(grid,i+1,j,n,m);
     }
 }
