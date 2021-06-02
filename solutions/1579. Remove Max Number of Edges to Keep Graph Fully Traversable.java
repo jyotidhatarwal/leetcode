915610d70cdec9cd761ecc1237277caf8dcec31c
Jyoti Dhatarwal <65608438+jyotidhatarwal@users.noreply.github.com>
2021-06-02 20:32:13 +0530

    47 ms

--- solutions/1579. Remove Max Number of Edges to Keep Graph Fully Traversable.java
+++ solutions/1579. Remove Max Number of Edges to Keep Graph Fully Traversable.java
@@ -1,37 +1,75 @@
-                else {
-                    if(Alice) cycleAlice++;
-                    else AliceComponent--;
-                    if(Bob) cycleBob++;
-                    else BobComponent--;
-                } 
-            } else if(edge[0]==2){
-                if(union(edge[1],edge[2],componentsBob,sizeBob))
-                    cycleBob++;
-                else BobComponent--;
-            } else {
-                if(union(edge[1],edge[2],componentsAlice,sizeAlice))
-                    cycleAlice++;
-                else AliceComponent--;
-            }
-        }
-        if(AliceComponent >1||BobComponent>1) return -1;
-        return cycleAlice+cycleBob+commonCycle;
-    }
-    
+class Solution {
     private int find(int i,int[] parent){
-        while(parent[i]!=i){
-            // compress path
-            i=parent[parent[i]];
+        if(parent[i] == i){
+            return i;
         }
-        return i;
+        int temp = find(parent[i],parent);
+        parent[i] = temp;
+        return temp;
     }
+    private boolean union(int x,int y,int[] size,int[] parent){
+        int lox = find(x,parent);
+        int loy = find(y,parent);
+        if(lox == loy) return false;
+        if(size[lox] < size[loy]){
+            parent[lox] = loy;
+            size[loy] += size[lox];
+        }else{
+            parent[loy] = lox;
+            size[lox] += size[loy];
+        }
+        return true;
+    }
+    public int maxNumEdgesToRemove(int n, int[][] edges) {
+        int[] AParent = new int[n+1];
+        int[] BParent = new int[n+1];
+        int[] ASize = new int[n+1];
+        int[] BSize = new int[n+1];
+        
+        int count =0;
+        
+        for(int i=0;i<=n;i++){
+            AParent[i] = i;
+            BParent[i] = i;
+            ASize[i] = 1;
+            BSize[i] = 1;
+        }
+        
+        Arrays.sort(edges,(a,b) ->(b[0] - a[0]));
+        for(int[] edge : edges){
+            if(edge[0] == 3){
+                 boolean alice = union(edge[1],edge[2],ASize,AParent);
+                boolean bob = union(edge[1],edge[2],BSize,BParent);
+                if(alice  && bob) count++;
+               
+            }else if(edge[0] == 2){
+              boolean bob =  union(edge[1],edge[2],BSize,BParent);
+               if(bob) count++;
+            }else{
+              boolean alice = union(edge[1],edge[2],ASize,AParent);
+                if(alice) count++;
+            }
+        }
+        
+        boolean presentA =false;
+       for(int i=0;i<=n;i++){
+           if(ASize[i] == n){
+               presentA=true;
+               break;
+           }
+       }
+        boolean presentB=false;
+        for(int i=0;i<=n;i++){
+           if(BSize[i] == n){
+               presentB=true;
+               break;
+           }
+       }
+        
+        
+        if(presentA && presentB)
+        return edges.length-count;
     
-    boolean union(int a,int b,int[] parent,int[] size){
-        int parentA=find(a,parent);
-        int parentB=find(b,parent);
-        // node a is already connected with node b. so this edge is redundant. 
-        if(parentA==parentB) return true;
-        // use size to determine which node can be parent 
-        if(size[parentA]>size[parentB]){
-            parent[parentB]=parentA;
-            size[parentA]+=size[parentB];
+       return -1;
+    }
+}
