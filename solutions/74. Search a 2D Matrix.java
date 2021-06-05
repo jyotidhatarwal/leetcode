c8adaf474365a31bf068754957fa825a2a3bacd4
Jyoti Dhatarwal <65608438+jyotidhatarwal@users.noreply.github.com>
2021-06-05 15:41:16 +0530

    0 ms

--- solutions/74. Search a 2D Matrix.java
+++ solutions/74. Search a 2D Matrix.java
@@ -1,21 +1,43 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        if(matrix.length ==0) return false;
-        int n = matrix.length;
-        int m = matrix[0].length;
-        int lo = 0;
-        int hi = (n*m)-1;
-        while(lo<= hi){
-            int mid = (lo + (hi-lo)/2);
-            if(matrix[mid/m][mid%m] == target){
-                return true;
-            }
-            if(matrix[mid/m][mid%m] > target){
-                hi = mid-1;
+       
+        int row = binarySearchRowSelect(matrix,target);
+       if(row == -1) return false;
+       boolean isFound = binarySearch(matrix,row,target);
+       
+       return isFound;
+    }
+    public  int binarySearchRowSelect(int[][]matrix,int target){
+        int low = 0;
+        int hi = matrix.length-1;
+        int lastColumn = matrix[0].length-1;
+        while(low <= hi){
+            int mid = (low + hi) /2;
+            
+            if(matrix[mid][0] <= target && target <= matrix[mid][lastColumn]){
+                return mid;
+            }else if(matrix[mid][0] < target){
+                low = mid +1;
             }else{
-                lo = mid+1;
+                hi = mid -1;
             }
         }
-        return false;
+        return -1;
     }
+    
+     public  boolean binarySearch(int[][]matrix,int row,int target){
+         int low = 0;
+         int hi = matrix[0].length-1;
+         while(low <= hi){
+             int mid = (low + hi) /2;
+             if(matrix[row][mid] == target){
+                 return true;
+             }else if(matrix[row][mid] < target){
+                 low = mid +1;
+             }else{
+                 hi = mid-1;
+             }
+         }
+         return false;
+     }
 }
