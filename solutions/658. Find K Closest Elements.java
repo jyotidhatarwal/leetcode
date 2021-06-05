888136c76198e2b7a985a845ba50d23f2f7bb376
Jyoti Dhatarwal <65608438+jyotidhatarwal@users.noreply.github.com>
2021-06-05 15:51:42 +0530

    39 ms

--- solutions/658. Find K Closest Elements.java
+++ solutions/658. Find K Closest Elements.java
@@ -2,12 +2,13 @@ class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
         ArrayList<Integer> list = new ArrayList<>();
         PriorityQueue<Pair> pq = new PriorityQueue<>(Collections.reverseOrder());
+        
         for(int i=0;i<arr.length;i++){
             if(pq.size() < k){
-                pq.add(new Pair(arr[i],Math.abs(arr[i] - x)));
-            }else if(pq.peek().gap > Math.abs(arr[i] - x)){
+                pq.add(new Pair(arr[i],Math.abs(arr[i]-x)));
+            }else if(pq.peek().gap > Math.abs(arr[i]-x)){
                 pq.remove();
-                pq.add(new Pair(arr[i],Math.abs(arr[i] - x)));
+                pq.add(new Pair(arr[i],Math.abs(arr[i]-x)));
             }
         }
         while(pq.size() > 0){
@@ -21,14 +22,14 @@ class Solution {
         int val;
         int gap;
         Pair(int val,int gap){
-            this.val= val;
-            this.gap = gap;
+            this.val=val;
+            this.gap=gap;
         }
         public int compareTo(Pair other){
             if(this.gap == other.gap){
                 return this.val - other.val;
             }else{
-                return this.gap - other.gap;
+                return this.gap- other.gap;
             }
         }
     }
