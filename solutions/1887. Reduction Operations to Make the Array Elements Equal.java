2df1af78403b60aa203e1ef1f26e728c3790a6c1
Jyoti Dhatarwal <65608438+jyotidhatarwal@users.noreply.github.com>
2021-06-06 10:43:54 +0530

    30 ms

---
+++ solutions/1887. Reduction Operations to Make the Array Elements Equal.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int reductionOperations(int[] nums) {
+        int count =0;
+        Arrays.sort(nums);
+        for(int i= nums.length-1;i>0;i--){
+            if(nums[i] == nums[i-1])continue;
+            else{
+                count += nums.length -i;
+            }
+        }
+        return count;
+    }
+}
