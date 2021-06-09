f8660d33b86590f473c50939693202c5e1380cb5
Jyoti Dhatarwal <65608438+jyotidhatarwal@users.noreply.github.com>
2021-06-09 19:57:09 +0530

    0 ms

--- solutions/968. Binary Tree Cameras.java
+++ solutions/968. Binary Tree Cameras.java
@@ -18,29 +18,25 @@
     // -1 i am covered by a camera .
     // 0 i am a camera.
     // 1 means i need a camera.
-class Solution{
-    int camera=0;
-    public int minCameraCover(TreeNode root) {
-        if(minCameraCover_(root) == 1) camera++;
-        return camera;
-    }
-    
-    public int minCameraCover_(TreeNode root){
-        if(root==null) return  -1;
+class Solution {
+     int cameras = 0;
+    public int minCamera(TreeNode root){
+        if(root == null) return 1;
         
-        int lres = minCameraCover_(root.left);
-        int rres = minCameraCover_(root.right);
-​
-        if(lres == 1 || rres == 1){
-            camera++;
+        int leftChild = minCamera(root.left);
+        int rightChild = minCamera(root.right);
+        
+        if(leftChild == -1 || rightChild == -1){
+            cameras++;
             return 0;
         }
-​
-        if(lres == 0 || rres == 0){
-            return -1;
-        }
-​
-        return 1;
-    }
-    
+        if(leftChild == 0 || rightChild == 0) return 1;
+        
+        return -1;
+    }
+    public int minCameraCover(TreeNode root) {
+        if(minCamera(root) == -1) cameras++;
+        return cameras;
+        
+    }
 }
