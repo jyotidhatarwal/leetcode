315ec8c9bc8a509f8b53337eeb07cd2b1e0eef68
Jyoti Dhatarwal <65608438+jyotidhatarwal@users.noreply.github.com>
2021-06-09 16:00:40 +0530

    3 ms

--- solutions/987. Vertical Order Traversal of a Binary Tree.java
+++ solutions/987. Vertical Order Traversal of a Binary Tree.java
@@ -14,32 +14,17 @@
  * }
  */
 class Solution {
-    public List<List<Integer>> verticalTraversal(TreeNode root) {
-        TreeMap<Integer, TreeMap<Integer, PriorityQueue<Integer>>> map = new TreeMap<>();
-        dfs(root, 0, 0, map);
-        List<List<Integer>> list = new ArrayList<>();
-        for (TreeMap<Integer, PriorityQueue<Integer>> ys : map.values()) {
-            list.add(new ArrayList<>());
-            for (PriorityQueue<Integer> nodes : ys.values()) {
-                while (!nodes.isEmpty()) {
-                    list.get(list.size() - 1).add(nodes.poll());
-                }
-            }
+    public class vPair{
+        TreeNode node = null;
+        int hl =0;
+        vPair(TreeNode node,int hl){
+            this.node = node;
+            this.hl =hl;
         }
-        return list;
     }
-    private void dfs(TreeNode root, int x, int y, TreeMap<Integer, TreeMap<Integer, PriorityQueue<Integer>>> map) {
-        if (root == null) {
-            return;
-        }
-        if (!map.containsKey(x)) {
-            map.put(x, new TreeMap<>());
-        }
-        if (!map.get(x).containsKey(y)) {
-            map.get(x).put(y, new PriorityQueue<>());
-        }
-        map.get(x).get(y).offer(root.val);
-        dfs(root.left, x - 1, y + 1, map);
-        dfs(root.right, x + 1, y + 1, map);
-    }
-}
+    public void width(TreeNode node,int hl,int[] ans){
+        if(node == null) return;
+        ans[0] = Math.min(ans[0],hl);
+        ans[1] = Math.max(ans[1],hl);
+        
+        width(node.left,hl-1,ans);
