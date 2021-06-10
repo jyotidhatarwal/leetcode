9ec65e5b3204cfc7c6022cb7a6383da88a4b5703
Jyoti Dhatarwal <65608438+jyotidhatarwal@users.noreply.github.com>
2021-06-10 10:24:49 +0530

    1 ms

--- solutions/98. Validate Binary Search Tree.java
+++ solutions/98. Validate Binary Search Tree.java
@@ -15,16 +15,20 @@
  */
 class Solution {
     public boolean isValidBST(TreeNode root) {
-       return help(root,Long.MIN_VALUE,Long.MAX_VALUE);
-        
-        
+        List<Integer> ans = new ArrayList<>();
+        inorder(root,ans);
+        for(int i=0;i<ans.size()-1;i++){
+            if(ans.get(i) >= ans.get(i+1)) return false;
+        }
+        return true;
     }
- private boolean help(TreeNode root,long min,long max){
-     if(root == null) return true ;
-     if(root.val > min && root.val < max){
-        return help(root.left,min,root.val) && help(root.right,root.val,max);
-     }
-     return false;
- }
-​
+    private List<Integer> inorder(TreeNode root,List<Integer> ans){
+        if(root == null){
+            return null;
+        }
+        inorder(root.left,ans);
+        ans.add(root.val);
+        inorder(root.right,ans);
+        return ans;
+    }
 }
