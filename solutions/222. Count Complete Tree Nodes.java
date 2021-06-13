c83ece85affda21a30aa874c2e6b0d101fcfdd68
Jyoti Dhatarwal <65608438+jyotidhatarwal@users.noreply.github.com>
2021-06-13 15:57:59 +0530

    0 ms

--- solutions/222. Count Complete Tree Nodes.java
+++ solutions/222. Count Complete Tree Nodes.java
@@ -15,10 +15,30 @@
  */
 class Solution {
     public int countNodes(TreeNode root) {
-        if(root == null) return 0;
-        
-        int left = countNodes(root.left);
-        int right = countNodes(root.right);
-        return left + right +1;
+        if(root == null){
+            return 0;
+        }
+        int leftHeight = leftCount(root);
+        int rightHeight = rightCount(root);
+        if(leftHeight == rightHeight){
+            return (1 << leftHeight) -1;
+        }
+        return countNodes(root.left) + countNodes(root.right) +1;
+    }
+    private int leftCount(TreeNode root){
+        int count=1;
+        while(root.left != null){
+            count++;
+            root = root.left;
+        }
+        return count;
+    }
+    private int rightCount(TreeNode root){
+        int count =1;
+        while(root.right != null){
+            count++;
+            root = root.right;
+        }
+        return count;
     }
 }
