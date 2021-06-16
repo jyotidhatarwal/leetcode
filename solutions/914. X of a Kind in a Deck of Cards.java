035030714361377741709641bcfd4223f44b3c55
Jyoti Dhatarwal <65608438+jyotidhatarwal@users.noreply.github.com>
2021-06-16 10:41:15 +0530

    7 ms

---
+++ solutions/914. X of a Kind in a Deck of Cards.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean hasGroupsSizeX(int[] deck) {
+        Map<Integer, Integer> count = new HashMap<>();
+        int res = 0;
+        for (int i : deck) count.put(i, count.getOrDefault(i, 0) + 1);
+        for (int i : count.values()) res = gcd(i, res);
+        if(res >=2) return true;
+        return false;
+    }
+​
+    public int gcd(int a, int b) {
+        return b > 0 ? gcd(b, a % b) : a;
+    
+    }
+}
+​
