e1d8c78dc891c3179a9050580433b2dea6da4fdb
Jyoti Dhatarwal <65608438+jyotidhatarwal@users.noreply.github.com>
2021-06-17 07:51:07 +0530

    2 ms

--- solutions/781. Rabbits in Forest.java
+++ solutions/781. Rabbits in Forest.java
@@ -1,17 +1,16 @@
 class Solution {
     public int numRabbits(int[] answers) {
         HashMap<Integer,Integer> map = new HashMap<>();
-        for(int val:answers){
+        for(int val : answers){
             map.put(val,map.getOrDefault(val,0)+1);
         }
-        int ans =0;
-        for(int key: map.keySet()){
+        int numberOfRabbitsInForest =0;
+        for(int key : map.keySet()){
             int groupSize = key +1;
-            int rabbitReported = map.get(key);
-            int numberOfGroups  =  (int)Math.ceil(rabbitReported*1.0 / groupSize*1.0);
-            ans += numberOfGroups * groupSize;
+            int rabbitsReported = map.get(key);
+            int numberOfGroups = (int) Math.ceil((rabbitsReported*1.0)/groupSize*1.0);
+            numberOfRabbitsInForest += numberOfGroups * groupSize;
         }
-        return ans;
-        
+        return numberOfRabbitsInForest;
     }
 }
