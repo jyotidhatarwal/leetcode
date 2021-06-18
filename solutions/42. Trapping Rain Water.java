e9772ff1ebc292e253ccd6008679370331b7fba7
Jyoti Dhatarwal <65608438+jyotidhatarwal@users.noreply.github.com>
2021-06-18 12:20:08 +0530

    1 ms

--- solutions/42. Trapping Rain Water.java
+++ solutions/42. Trapping Rain Water.java
@@ -1,21 +1,23 @@
 class Solution {
     public int trap(int[] height) {
-        int result = 0;
-        int start = 0;
-        int end = height.length - 1;
-        while (start < end) {
-            if (height[start] <= height[end]) {
-                int current = height[start];
-                while (height[++start] < current) {
-                    result += current - height[start];
-                }
-            } else {
-                int current = height[end];
-                while(height[--end] < current) {
-                    result += current - height[end];
-                }
-            }
+        int n = height.length;
+        if(n == 0){
+            return 0;
         }
-        return result;
+        int leftMax[] = new int[n];
+        int rightMax[] = new int[n];
+        leftMax[0] = height[0];
+        for(int i=1;i<n;i++){
+            leftMax[i] = Math.max(leftMax[i-1],height[i]);
+        }
+        rightMax[n-1] = height[n-1];
+        for(int i= n-2;i>=0;i--){
+            rightMax[i] = Math.max(height[i],rightMax[i+1]);
+        }
+        int ans =0;
+        for(int i=1;i<n-1;i++){
+            ans += Math.min(leftMax[i],rightMax[i]) - height[i];
+        }
+        return ans;
     }
 }
