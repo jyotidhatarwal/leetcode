4121da1731f7dc861b0fbb59afe1751ebe2635c6
Jyoti Dhatarwal <65608438+jyotidhatarwal@users.noreply.github.com>
2021-06-18 11:18:33 +0530

    6 ms

--- solutions/205. Isomorphic Strings.java
+++ solutions/205. Isomorphic Strings.java
@@ -3,21 +3,25 @@ class Solution {
         if(s.length() != t.length()){
             return false;
         }
+        // this is used to map character of s to t
         HashMap<Character,Character> map1 = new HashMap<>();
+        
+        // this is used to mark character of t i.e are they used or not
         HashMap<Character,Boolean> map2 = new HashMap<>();
+        
         for(int i=0;i<s.length();i++){
-            char ch1 = s.charAt(i);
-            char ch2 = t.charAt(i);
-            if(map1.containsKey(ch1) == true){
-                if(map1.get(ch1) != ch2){
+            char c1 = s.charAt(i);
+            char c2 = t.charAt(i);
+            if(map1.containsKey(c1) == true){
+                if(map1.get(c1) != c2){
                     return false;
                 }
             }else{
-                if(map2.containsKey(ch2) == true){
+                if(map2.containsKey(c2) == true){
                     return false;
                 }else{
-                    map1.put(ch1,ch2);
-                    map2.put(ch2,true);
+                    map1.put(c1,c2);
+                    map2.put(c2,true);
                 }
             }
         }
