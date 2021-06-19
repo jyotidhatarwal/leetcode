54c6f381af95c25d7561300685935184d2801453
Jyoti Dhatarwal <65608438+jyotidhatarwal@users.noreply.github.com>
2021-06-19 20:42:50 +0530

    9 ms

--- solutions/554. Brick Wall.java
+++ solutions/554. Brick Wall.java
@@ -1,18 +1,17 @@
 class Solution {
     public int leastBricks(List<List<Integer>> wall) {
-        int n = wall.size();
-        int count =0;
+        int noOfRows = wall.size();
+       int maxEmptySpace =0;
         HashMap<Integer,Integer> map = new HashMap<>();
         for(List<Integer> row : wall){
             int prefixSum =0;
             for(int j=0;j<row.size()-1;j++){
                 prefixSum += row.get(j);
                 map.put(prefixSum,map.getOrDefault(prefixSum,0)+1);
-                count = Math.max(count,map.get(prefixSum));
+               maxEmptySpace = Math.max(maxEmptySpace,map.get(prefixSum));
             }
         }
-       
-        int ans = n- count;
-        return ans;
+        int minBricksToCross = noOfRows - maxEmptySpace;
+        return  minBricksToCross;
     }
 }
