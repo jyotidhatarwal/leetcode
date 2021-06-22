f39587ddb2e323b489fbfa37bb3e7964dd5e6596
Jyoti Dhatarwal <65608438+jyotidhatarwal@users.noreply.github.com>
2021-06-22 17:52:52 +0530

    19 ms

--- solutions/525. Contiguous Array.java
+++ solutions/525. Contiguous Array.java
@@ -1,20 +1,21 @@
 class Solution {
     public int findMaxLength(int[] nums) {
-        int ans=0;
+        int ans =0;
+                // sum, index
         HashMap<Integer,Integer> map = new HashMap<>();
-        map.put(0,-1);
         int sum =0;
+        map.put(sum,-1);
         for(int i=0;i<nums.length;i++){
-            if(nums[i] ==0){
+            if(nums[i] == 0){
                 sum += -1;
-            }else if(nums[i] == 1){
-                sum += 1;
+            }else{
+                sum += nums[i];
             }
             if(map.containsKey(sum)){
                 int idx = map.get(sum);
-                int length = i -idx;
-                if(length > ans){
-                    ans = length;
+                int len = i - idx;
+                if(len > ans){
+                    ans = len;
                 }
             }else{
                 map.put(sum,i);
