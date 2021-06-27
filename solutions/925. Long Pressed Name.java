9f1ca1ba7307391842a31961d129f8f391d13c44
Jyoti Dhatarwal <65608438+jyotidhatarwal@users.noreply.github.com>
2021-06-27 07:12:48 +0530

    0 ms

--- solutions/925. Long Pressed Name.java
+++ solutions/925. Long Pressed Name.java
@@ -1,41 +1,29 @@
 class Solution {
     public boolean isLongPressedName(String name, String typed) {
-​
-        // two pointers to the "name" and "typed" string respectively
-        int np = 0, tp = 0;
-        // convert the string to array of chars, for ease of processing later.
-        char[] name_chars = name.toCharArray();
-        char[] typed_chars = typed.toCharArray();
-​
-        // advance two pointers, until we exhaust one of the strings
-        while (np < name_chars.length && tp < typed_chars.length) {
-            if (name_chars[np] == typed_chars[tp]) {
-                np += 1;
-                tp += 1;
-            } else if (tp >= 1 && typed_chars[tp] == typed_chars[tp - 1]) {
-                tp += 1;
-            } else {
+        if(name.length() > typed.length()){
+            return false;
+        }
+        int i=0;
+        int j=0;
+        while(i < name.length() && j < typed.length()){
+            if(name.charAt(i) == typed.charAt(j)){
+                i++;
+                j++;
+            }else if(i > 0 && name.charAt(i-1) == typed.charAt(j)){
+                j++;
+            }else{
                 return false;
             }
         }
-​
-        // if there is still some characters left *unmatched* in the origin string,
-        // then we don't have a match.
-        // e.g. name = "abc" typed = "aabb"
-        if (np != name_chars.length) {
-            return false;
-        } else {
-            // In the case that there are some redundant characters left in typed
-            // we could still have a match.
-            // e.g. name = "abc" typed = "abccccc"
-            while (tp < typed_chars.length) {
-                if (typed_chars[tp] != typed_chars[tp - 1])
-                    return false;
-                tp += 1;
+        while(j < typed.length()){
+            if(name.charAt(i-1) != typed.charAt(j)){
+                return false;
             }
+            j++;
         }
-​
-        // both strings have been consumed.
+        if(i < name.length()){
+            return false;
+        }
         return true;
     }
 }
