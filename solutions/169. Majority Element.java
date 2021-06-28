9128025a6e852a7d0463b397ce52edbe4ab1c689
Jyoti Dhatarwal <65608438+jyotidhatarwal@users.noreply.github.com>
2021-06-28 07:42:26 +0530

    2 ms

--- solutions/169. Majority Element.java
+++ solutions/169. Majority Element.java
@@ -1,20 +1,33 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int ansIndex =0;
+        int value = validCandidate(nums);
+        int count =0;
+        for(int i=0;i<nums.length;i++){
+            if(value == nums[i]){
+                count++;
+            }
+        }
+        // if(count > nums.length/2){
+        //     return value;
+        // }
+        return value;
+    }
+    private int validCandidate(int[]nums){
+        int val = nums[0];
         int count =1;
         for(int i=1;i<nums.length;i++){
-            if(nums[i] == nums[ansIndex]){
+            if(val == nums[i]){
+                // same value
                 count++;
             }else{
+                // distinct elements, mapping of them
                 count--;
             }
-            if(count==0){
-                ansIndex =i;
+            if(count == 0){
+                val = nums[i];
                 count =1;
             }
         }
-        return nums[ansIndex];
-       
-        
+        return val;
     }
 }
