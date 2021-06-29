d5272c1cd5dffbe5026802805b861c188431f678
Jyoti Dhatarwal <65608438+jyotidhatarwal@users.noreply.github.com>
2021-06-29 20:35:26 +0530

    0 ms

---
+++ solutions/119. Pascal's Triangle II.java
@@ -0,0 +1,14 @@
+class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        //  using nCr and factor i.e nCr*factor = nCr+1
+        // nC0 =1
+        List<Integer> result = new ArrayList<>();
+        int val =1;
+        for(int j=0;j<=rowIndex;j++){
+            result.add(val);
+            long cv=(long)val*(long)(rowIndex-j)/(j+1);
+            val = (int)cv;
+        }
+        return result;
+    }
+}
