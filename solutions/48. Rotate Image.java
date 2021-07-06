e8d5fd5a531692e5a777f9f6ba14fd00772db62d
Jyoti Dhatarwal <65608438+jyotidhatarwal@users.noreply.github.com>
2021-07-06 07:47:57 +0530

    0 ms

--- solutions/48. Rotate Image.java
+++ solutions/48. Rotate Image.java
@@ -1,32 +1,31 @@
 class Solution {
     public void rotate(int[][] matrix) {
-        // step-1 transpose
         transpose(matrix);
-        // step-2 reverse every row
-        reverseRow(matrix);
+        reverseEveryRow(matrix);
     }
-    private void transpose(int[][] arr){
-        int n = arr.length;
+    private void transpose(int[][]matrix){
+        int n = matrix.length;
         for(int i=0;i<n;i++){
             for(int j=0;j<i;j++){
-                swap(arr,i,j);
+                swap(matrix,i,j);
             }
         }
     }
-    private void swap(int[][] arr,int i,int j){
-        int temp = arr[i][j];
-        arr[i][j] = arr[j][i];
-        arr[j][i] = temp;
+    private void swap(int[][]matrix,int i,int j){
+        int temp = matrix[i][j];
+        matrix[i][j] = matrix[j][i];
+        matrix[j][i] = temp;
     }
-    private void reverseRow(int[][] arr){
-        int n = arr.length;
-        for(int row =0;row<n;row++){
-            int left =0;
-            int right = n-1;
+    private void reverseEveryRow(int[][]matrix){
+        int n = matrix.length;
+      
+        for(int row=0;row<n;row++){
+             int left =0;
+             int right = n-1; 
             while(left < right){
-                int temp = arr[row][left];
-                arr[row][left] = arr[row][right];
-                arr[row][right] = temp;
+                int temp = matrix[row][left];
+                matrix[row][left] = matrix[row][right];
+                matrix[row][right] = temp;
                 left++;
                 right--;
             }
