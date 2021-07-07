ff6ee121a395b72102db146e2f689c818977ce06
Jyoti Dhatarwal <65608438+jyotidhatarwal@users.noreply.github.com>
2021-07-07 18:44:55 +0530

    1 ms

---
+++ solutions/202. Happy Number.java
@@ -0,0 +1,42 @@
+class Solution {
+    public boolean isHappy(int n) {
+      // boolean result = false;
+      //   while(true){
+      //      int ans = squareSum(n);
+      //       if(ans == 1){
+      //           result = true;
+      //           break;
+      //       }else{
+      //           n = squareSum(n);
+      //       }
+      //   }
+      //   return result;
+        
+        HashMap<Integer,Integer> map = new HashMap<>();
+        boolean result = false;
+        while(n >=1){
+            int ans = squareSum(n);
+            map.put(ans,map.getOrDefault(ans,0)+1);
+            if(ans == 1){
+                result = true;
+                break;
+            }else{
+                n = squareSum(n);
+                if(map.get(n) > 1){
+                    result = false;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+    private int squareSum(int n){
+        int ans =0;
+        while(n > 0){
+            int temp = n % 10;
+            ans += (temp*temp);
+            n = n/10;
+        }
+        return ans;
+    }
+}
