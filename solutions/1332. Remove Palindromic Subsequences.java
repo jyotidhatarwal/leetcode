013c743832e03632dd795f4e04f683b30ac37b15
Jyoti Dhatarwal <65608438+jyotidhatarwal@users.noreply.github.com>
2021-07-07 13:42:19 +0530

    0 ms

--- solutions/1332. Remove Palindromic Subsequences.java
+++ solutions/1332. Remove Palindromic Subsequences.java
@@ -1,29 +1,26 @@
 class Solution {
-​
-    // TC : O(n)
-    // SC : O(1), n no of char in the input sttr
     public int removePalindromeSub(String s) {
         if(s.length() == 0){
             return 0;
-        } else if(checkPalindrome(s)){
+        }
+        if(isPalindrome(s) == true){
             return 1;
-        } else{
+        }else{
             return 2;
         }
     }
-​
-    private boolean checkPalindrome(String s){
-        int i=0;
-        int j = s.length() -1;
-        while(i<j){
-            if(s.charAt(i) == s.charAt(j)){
-                i++;
-                j--;
-            } else{
+    private boolean isPalindrome(String s){
+        
+        int left =0;
+        int right = s.length()-1;
+        while(left < right){
+            if(s.charAt(left) == s.charAt(right)){
+                left++;
+                right--;
+            }else{
                 return false;
             }
         }
-​
         return true;
     }
 }
