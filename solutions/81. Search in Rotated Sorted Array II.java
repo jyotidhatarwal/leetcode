b7060ffe28c7d23773ccdae9acdf5fee6d40248e
Jyoti Dhatarwal <65608438+jyotidhatarwal@users.noreply.github.com>
2021-07-14 00:16:58 +0530

    1 ms

--- solutions/81. Search in Rotated Sorted Array II.java
+++ solutions/81. Search in Rotated Sorted Array II.java
@@ -1,12 +1,35 @@
 class Solution {
     public boolean search(int[] nums, int target) {
-        boolean result = false;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i] == target){
-                result =true;
-                break;
+        
+        int low = 0;
+        int hi = nums.length-1;
+        
+        while(low <= hi){
+            
+            while(low < hi && nums[low] == nums[low+1]){
+                low++;
+            }
+            while(low < hi && nums[hi] == nums[hi-1]){
+                hi--;
+            }
+            int mid = low + (hi - low)/2;
+            
+            if(nums[mid] == target){
+                return true;
+            }else if(nums[low] <= nums[mid]){
+                if(target >= nums[low] && nums[mid] > target){
+                    hi = mid-1;
+                }else{
+                    low = mid+1;
+                }
+            }else if(nums[mid] <= nums[hi]){
+                if(target > nums[mid] && nums[hi] >= target){
+                    low = mid+1;
+                }else{
+                    hi = mid-1;
+                }
             }
         }
-        return result;
+        return false;
     }
 }
