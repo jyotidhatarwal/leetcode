254694e9b31398c0d9f99529dee8faab031649da
Jyoti Dhatarwal <65608438+jyotidhatarwal@users.noreply.github.com>
2021-07-22 10:43:32 +0530

    221 ms

--- solutions/354. Russian Doll Envelopes.java
+++ solutions/354. Russian Doll Envelopes.java
@@ -1,33 +1,24 @@
-    
-    private int LIS(int[][] arr){
-          int ans=0;
-          int[] minele=new int[arr.length];
-        for(int i=0;i<arr.length;i++){
-            int val=arr[i][1];
-            int lo=0, hi=ans;
-            
-            while(lo<hi){
-                int mid=(lo + hi)/2;
-                if(minele[mid]<val){
-                    lo=mid+1;
-                }else{
-                    hi=mid;
+class Solution {
+    public int maxEnvelopes(int[][] envelopes) {
+        Arrays.sort(envelopes,(a,b)->{
+            return a[0]-b[0];
+        });
+        int[]dp = new int[envelopes.length];
+        int ans =0;
+        for(int i=0;i<envelopes.length;i++){
+            int max =0;
+            for(int j=0;j<i;j++){
+                if(envelopes[i][1] > envelopes[j][1] && envelopes[i][0] > envelopes[j][0]){
+                    if(dp[j] > max){
+                        max = dp[j];
+                    }
                 }
             }
-            minele[lo]=val;
-            
-            if(lo==ans){
-                ans++;
+            dp[i] = max +1;
+            if(dp[i] > ans){
+                ans = dp[i];
             }
         }
         return ans;
-    }
-    
-    public class EnvelopeComparator implements Comparator<int[]>{
-        @Override
-        public int compare(int[] one,int[] two){
-            if(one[0]==two[0]) return two[1]-one[1];
-            return one[0]-two[0];
-        }
     }
 }
