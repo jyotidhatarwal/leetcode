3a09309c13c5bc7496f0ec5b4e17529e688d67fc
Jyoti Dhatarwal <65608438+jyotidhatarwal@users.noreply.github.com>
2021-07-22 07:22:50 +0530

    60 ms

--- solutions/300. Longest Increasing Subsequence.java
+++ solutions/300. Longest Increasing Subsequence.java
@@ -1,21 +1,23 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int[]dp = new int[nums.length];
-        int ans =0;
-        for(int val : nums){
-            int i =0;
-            int j = ans;
-            while(i < j){
-                int mid = i + (j-i)/2;
-                if(dp[mid] < val){
-                    i = mid+1;
-                }else{
-                    j = mid;
+        int n = nums.length;
+        int[] dp = new int[n];
+        dp[0] =1;
+        for(int i=1;i<n;i++){
+            int max =0;
+            for(int j=0;j<i;j++){
+                if(nums[i] > nums[j]){
+                    if(dp[j] > max){
+                        max = dp[j];
+                    }
                 }
             }
-            dp[i] = val;
-            if(i == ans){
-                ans++;
+            dp[i] = max +1;
+        }
+        int ans =0;
+        for(int i=0;i<n;i++){
+            if(dp[i] > ans){
+                ans = dp[i];
             }
         }
         return ans;
