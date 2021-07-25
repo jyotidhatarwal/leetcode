a3b1b84c1be6b74b79e6774f7e94511cf972f5e1
Jyoti Dhatarwal <65608438+jyotidhatarwal@users.noreply.github.com>
2021-07-25 18:14:15 +0530

    12 ms

--- solutions/647. Palindromic Substrings.java
+++ solutions/647. Palindromic Substrings.java
@@ -1,23 +1,30 @@
 class Solution {
-    private boolean isPalindrome(String s, int start, int end) {
-        while (start < end) {
-            if (s.charAt(start) != s.charAt(end)) 
-                return false;
-​
-            ++start;
-            --end;
-        }
-​
-        return true;
-    }
-​
     public int countSubstrings(String s) {
-        int ans = 0;
-​
-        for (int start = 0; start < s.length(); ++start)
-            for (int end = start; end < s.length(); ++end) 
-                ans += isPalindrome(s, start, end) ? 1 : 0;
-​
-        return ans;
+        int n = s.length();
+        boolean[][]dp = new boolean[n][n];
+        int count =0;
+        for(int gap =0;gap<n;gap++){
+            for(int i=0,j=gap;j<n;i++,j++){
+                if(gap == 0){
+                    dp[i][j] = true;
+                    count++;
+                }else if(gap == 1){
+                    if(s.charAt(i) == s.charAt(j)){
+                        dp[i][j] = true;
+                        count++;
+                    }else{
+                        dp[i][j] = false;
+                    }
+                }else{
+                    if(s.charAt(i) == s.charAt(j) && dp[i+1][j-1] == true){
+                        dp[i][j] = true;
+                        count++;
+                    }else{
+                        dp[i][j] = false;
+                    }
+                }
+            }
+        }
+        return count;
     }
 }
