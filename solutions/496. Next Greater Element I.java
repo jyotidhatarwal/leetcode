1f4bf50985f74b564e241ed370d794ac04218199
Jyoti Dhatarwal <65608438+jyotidhatarwal@users.noreply.github.com>
2021-08-03 11:45:01 +0530

    3 ms

--- solutions/496. Next Greater Element I.java
+++ solutions/496. Next Greater Element I.java
@@ -1,25 +1,33 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int [] ans = new int[nums1.length];
-        int[] nge = nextGreaterElementRight(nums2);
+        int[]ans = new int[nums1.length];
+        int[]nge2 = nextGreater(nums2);
         HashMap<Integer,Integer> map = new HashMap<>();
         for(int i=0;i<nums2.length;i++){
-            map.put(nums2[i],nge[i]);
+            map.put(nums2[i],nge2[i]);
         }
         for(int i=0;i<nums1.length;i++){
             ans[i] = map.get(nums1[i]);
         }
         return ans;
+        
     }
-    
-     private int[] nextGreaterElementRight(int[] nums2) {
+    private int[] nextGreater(int[]nums){
+        int[]nge = new int[nums.length];
         Stack<Integer> st = new Stack<>();
-         int[] ans = new int[nums2.length];
-         for(int i=nums2.length-1;i>=0;i--){
-             while(st.size() > 0 && st.peek() < nums2[i]) st.pop();
-             ans[i] = st.size() > 0 ? st.peek() : -1;
-             st.push(nums2[i]);
-         }
-         return ans;
+        st.push(nums[nums.length-1]);
+        nge[nums.length-1] = -1;
+        for(int i= nums.length-2;i>=0;i--){
+            while(st.size() > 0 && nums[i] >= st.peek()){
+                st.pop();
+            }
+            if(st.size() == 0){
+                nge[i] = -1;
+            }else{
+                nge[i] = st.peek();
+            }
+            st.push(nums[i]);
+        }
+        return nge;
     }
 }
