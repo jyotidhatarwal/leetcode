c8342bf60d61a1c30a5d5b4c2fe1f462481d3e2b
Jyoti Dhatarwal <65608438+jyotidhatarwal@users.noreply.github.com>
2021-08-03 07:53:17 +0530

    6 ms

--- solutions/3. Longest Substring Without Repeating Characters.java
+++ solutions/3. Longest Substring Without Repeating Characters.java
@@ -1,44 +1,43 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        int ans=0;
-        HashMap<Character,Integer>map = new HashMap<>();
-        int i=-1;
-        int j=-1;
-        // acquire
+        int ans =0;
+        HashMap<Character,Integer> map = new HashMap<>();
+        int i= 0;
+        int j =0;
         while(true){
-            boolean f1 = false;
-            boolean f2 = false;
-            while(i<s.length()-1){
-                f1 =true;
-                i++;
-                char ch = s.charAt(i);
+            boolean flag1 = false;
+            boolean flag2 = false;
+            // acquire
+            while(j < s.length()){
+                flag1 = true;
+                char ch = s.charAt(j);
                 map.put(ch,map.getOrDefault(ch,0)+1);
-                if(map.get(ch)==2){
+                if(map.get(ch) ==2){
+                    j++;
                     break;
                 }else{
-                    int length = i-j;
+                    int length = j-i+1;
                     if(length > ans){
                         ans = length;
                     }
                 }
+                j++;
             }
             // release
-            while(j<i){
-                f2 = true;
-                j++;
-                char ch = s.charAt(j);
+            while(i < j){
+                flag2 = true;
+                char ch =s.charAt(i);
                 map.put(ch,map.get(ch)-1);
-                if(map.get(ch)==1){
+                if(map.get(ch) == 1){
+                    i++;
                     break;
                 }
+                i++;
             }
-            if(f1 == false && f2 == false){
+            if(flag1 == false && flag2 == false){
                 break;
             }
         }
-        
-        
         return ans;
-        
     }
 }
