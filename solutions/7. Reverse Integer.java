55cf9ff1f40376495135833656861d9b12973582
Jyoti Dhatarwal <65608438+jyotidhatarwal@users.noreply.github.com>
2021-08-03 10:21:31 +0530

    1 ms

--- solutions/7. Reverse Integer.java
+++ solutions/7. Reverse Integer.java
@@ -2,12 +2,12 @@ class Solution {
     public int reverse(int x) {
       int val=0;
         while(x != 0){
-            int r = x%10;
+            int remainder  = x % 10;
             if(val > Integer.MAX_VALUE/10 || val < Integer.MIN_VALUE/10){
                 return 0;
             }
-            val = val*10 +r;
-            x /= 10;
+            val = val*10 + remainder;
+            x = x / 10;
         }
         return val;
     }
