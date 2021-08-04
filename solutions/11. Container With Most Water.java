61413102f0f8239352fe8ee466b8dce7815900f3
Jyoti Dhatarwal <65608438+jyotidhatarwal@users.noreply.github.com>
2021-08-04 11:48:26 +0530

    3 ms

--- solutions/11. Container With Most Water.java
+++ solutions/11. Container With Most Water.java
@@ -2,13 +2,11 @@ class Solution {
     public int maxArea(int[] height) {
         int i=0;
         int j = height.length-1;
-        int water =0;
+        int water = 0;
         while(i < j){
             int width = j-i;
-            int heights = Math.min(height[i],height[j]);
-            
-            water = Math.max(water,heights*width);
-            
+            int Height = Math.min(height[i],height[j]);
+            water = Math.max(water,width*Height);
             if(height[i] < height[j]){
                 i++;
             }else{
