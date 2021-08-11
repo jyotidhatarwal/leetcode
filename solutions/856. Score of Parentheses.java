a072c99b395ad35465e5ab7dce4546e99e99f76e
Jyoti Dhatarwal <65608438+jyotidhatarwal@users.noreply.github.com>
2021-08-11 07:01:43 +0530

    1 ms

--- solutions/856. Score of Parentheses.java
+++ solutions/856. Score of Parentheses.java
@@ -1,16 +1,16 @@
 class Solution {
     public int scoreOfParentheses(String s) {
         Stack<Integer> st = new Stack<>();
-        // '(' --> -1
         for(int i=0;i<s.length();i++){
-            if(s.charAt(i) == '('){
+            char ch = s.charAt(i);
+            if(ch == '('){
                 st.push(-1);
             }else{
                 if(st.peek() == -1){
                     st.pop();
                     st.push(1);
                 }else{
-                    int val=0;
+                    int val =0;
                     while(st.peek() != -1){
                         val += st.pop();
                     }
