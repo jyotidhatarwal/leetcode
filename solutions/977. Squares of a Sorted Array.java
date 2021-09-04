be0bc43f82e13525f6f97a1b756ae4496a9b2fe6
Jyoti Dhatarwal <65608438+jyotidhatarwal@users.noreply.github.com>
2021-09-04 18:37:51 +0530

    2 ms

--- solutions/977. Squares of a Sorted Array.java
+++ solutions/977. Squares of a Sorted Array.java
@@ -1,21 +1,20 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        int[]res = new int[nums.length];
-        int i=0;
+        int[]ans = new int[nums.length];
+        int i = 0;
         int j = nums.length-1;
-        int idx = res.length-1;
-        while(i <= j){
-            int val1 = nums[i]*nums[i];
-            int val2 = nums[j]*nums[j];
-            if(val1 > val2){
-                res[idx] = val1;
+        int k = nums.length-1;
+        while(i<=j){
+            if(Math.abs(nums[i]) > Math.abs(nums[j])){
+                ans[k] = nums[i]*nums[i];
+                k--;
                 i++;
             }else{
-                res[idx] = val2;
+                ans[k] = nums[j]*nums[j];
+                k--;
                 j--;
             }
-            idx--;
         }
-        return res;
+        return ans;
     }
 }
