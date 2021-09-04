79e26f10472f4e16f7ec7edf07dba8fc6d108283
Jyoti Dhatarwal <65608438+jyotidhatarwal@users.noreply.github.com>
2021-09-04 19:58:14 +0530

    1 ms

--- solutions/53. Maximum Subarray.java
+++ solutions/53. Maximum Subarray.java
@@ -3,14 +3,12 @@ class Solution {
         int currentSum = nums[0];
         int overallSum = nums[0];
         for(int i=1;i<nums.length;i++){
-            if(currentSum >0){
+            if(currentSum >= 0){
                 currentSum += nums[i];
             }else{
                 currentSum = nums[i];
             }
-            if(currentSum > overallSum){
-                overallSum = currentSum;
-            }
+            overallSum = Math.max(currentSum,overallSum);
         }
         return overallSum;
     }
