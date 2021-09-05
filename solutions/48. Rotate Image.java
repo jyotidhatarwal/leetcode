4e43c4ba70a030641744f5fed36aa6214be3af82
Jyoti Dhatarwal <65608438+jyotidhatarwal@users.noreply.github.com>
2021-09-05 16:46:41 +0530

    0 ms

--- solutions/48. Rotate Image.java
+++ solutions/48. Rotate Image.java
@@ -1,27 +1,21 @@
 class Solution {
     public void rotate(int[][] matrix) {
         transpose(matrix);
-        reverseEveryRow(matrix);
+        reverseRow(matrix);
     }
     private void transpose(int[][]matrix){
-        int n = matrix.length;
-        for(int i=0;i<n;i++){
+        for(int i=0;i<matrix.length;i++){
             for(int j=0;j<i;j++){
-                swap(matrix,i,j);
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
             }
         }
     }
-    private void swap(int[][]matrix,int i,int j){
-        int temp = matrix[i][j];
-        matrix[i][j] = matrix[j][i];
-        matrix[j][i] = temp;
-    }
-    private void reverseEveryRow(int[][]matrix){
-        int n = matrix.length;
-      
-        for(int row=0;row<n;row++){
-             int left =0;
-             int right = n-1; 
+    private void reverseRow(int[][]matrix){
+        for(int row =0;row<matrix.length;row++){
+            int left = 0;
+            int right = matrix.length-1;
             while(left < right){
                 int temp = matrix[row][left];
                 matrix[row][left] = matrix[row][right];
