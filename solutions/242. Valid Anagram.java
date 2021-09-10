b30efabab5bc79a105cd460d4d757d580075be35
Jyoti Dhatarwal <65608438+jyotidhatarwal@users.noreply.github.com>
2021-09-10 08:25:40 +0530

    25 ms

--- solutions/242. Valid Anagram.java
+++ solutions/242. Valid Anagram.java
@@ -1,23 +1,22 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
+        boolean result = true;
+        if(s.length() != t.length()){
+            return false;
+        }
         HashMap<Character,Integer> map = new HashMap<>();
         for(int i=0;i<s.length();i++){
-            char ch = s.charAt(i);
-            map.put(ch,map.getOrDefault(ch,0)+1);
+            map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)+1);
         }
         for(int i=0;i<t.length();i++){
             char ch = t.charAt(i);
-            if(map.containsKey(ch) == false){
-                return false;
-            }else if(map.get(ch) == 1){
-                map.remove(ch);
-            }else{
+            if(map.getOrDefault(ch,0) > 0){
                 map.put(ch,map.get(ch)-1);
+            }else{
+                result = false;
+                break;
             }
         }
-        if(map.size() != 0){
-            return false;
-        }
-        return true;
+        return result;
     }
 }
