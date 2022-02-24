851c65b0809a7fa5845bf79daf82cebd8a99d4b0
Jyoti Dhatarwal <65608438+jyotidhatarwal@users.noreply.github.com>
2022-02-24 09:18:06 +0530

    3 ms

--- solutions/283. Move Zeroes.java
+++ solutions/283. Move Zeroes.java
@@ -1,23 +1,34 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int n = nums.length;
-        if(n==0 || n==1){
-            return;
-        }
-        int left=0;
-        int right=0;
-        int temp;
-        while(right<n){
-            if(nums[right]==0){
-                right++;
-            }else{
-                temp=nums[left];
-                nums[left] = nums[right];
-                nums[right] = temp;
-                right++;
-                left++;
+//         int n = nums.length;
+//         if(n==0 || n==1){
+//             return;
+//         }
+//         int left=0;
+//         int right=0;
+//         int temp;
+//         while(right<n){
+//             if(nums[right]==0){
+//                 right++;
+//             }else{
+//                 temp=nums[left];
+//                 nums[left] = nums[right];
+//                 nums[right] = temp;
+//                 right++;
+//                 left++;
+//             }
+//         }
+     
+        int[]ans = new int[nums.length];
+        int k =0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i] != 0){
+                ans[k] = nums[i];
+                k++;
             }
         }
-        
+        for(int i=0;i<nums.length;i++){
+            nums[i] = ans[i];
+        }
     }
 }
