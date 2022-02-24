63f19a057be66113be9574a6e7bc073b58b02d98
Jyoti Dhatarwal <65608438+jyotidhatarwal@users.noreply.github.com>
2022-02-24 17:56:13 +0530

    10 ms

--- solutions/148. Sort List.java
+++ solutions/148. Sort List.java
@@ -9,37 +9,40 @@
  * }
  */
 class Solution {
-    private ListNode midOfLL(ListNode node){
-        if(node == null || node.next == null)return node;
-        ListNode fast = node;
-        ListNode slow = node;
+    public ListNode sortList(ListNode head) {
+       if(head == null || head.next == null){
+           return head;
+       }
+        ListNode mid = middle(head);
+        ListNode newHead = mid.next;
+        mid.next = null;
+        ListNode l1 = sortList(head);
+        ListNode l2 = sortList(newHead);
+        return mergeTwoLL(l1,l2);
+    }
+    private ListNode middle(ListNode head){
+        if(head == null || head.next == null){
+            return head;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
         while(fast.next != null && fast.next.next != null){
-            fast = fast.next.next;
             slow = slow.next;
+            fast = fast.next.next;
         }
         return slow;
     }
-    private ListNode reverse(ListNode node){
-        if(node == null || node.next == null) return node;
-        ListNode prev = null;
-        ListNode curr = node;
-        while(curr != null){
-            ListNode next = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = next;
+    private ListNode mergeTwoLL(ListNode l1,ListNode l2){
+        if(l1 == null || l2 == null){
+            return l1 != null ? l1 : l2;
         }
-        return prev;
-    }
-    private ListNode mergeTwoList(ListNode l1,ListNode l2){
-        if(l1 == null || l2 == null) return l1 != null ? l1 :l2;
         ListNode dummy = new ListNode(-1);
-        ListNode prev = dummy;
         ListNode c1 = l1;
-        ListNode c2 =l2;
+        ListNode c2 = l2;
+        ListNode prev = dummy;
         while(c1 != null && c2 != null){
-            if(c1.val <c2.val){
-                prev.next =c1;
+            if(c1.val < c2.val){
+                prev.next = c1;
                 c1 = c1.next;
             }else{
                 prev.next = c2;
@@ -49,15 +52,5 @@ class Solution {
         }
         prev.next = c1 != null ? c1 : c2;
         return dummy.next;
-    }
-    public ListNode sortList(ListNode head) {
-        if(head == null || head.next == null) return head;
-        ListNode mid = midOfLL(head);
-        ListNode newHead = mid.next;
-        mid.next =null;
-        ListNode l1 = sortList(head);
-        ListNode l2 = sortList(newHead);
-        return mergeTwoList(l1,l2);
-        
     }
 }
