eb01071f63bb874ab5de7488d9dd8cb20dda5d76
Jyoti Dhatarwal <65608438+jyotidhatarwal@users.noreply.github.com>
2026-01-10 17:04:32 +0530

    Time: 4 ms (49.81%) | Memory: 54.8 MB (93.23%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    let n = s.length;
+    let stack = [];
+    for(let i=0;i<n;i++){
+        if(s[i] === '(' || s[i] === '{' || s[i] === '['){
+            stack.push(s[i]);
+        }else if(s[i] === ')'){
+            if(stack[stack.length - 1] === '('){
+                stack.pop();
+            }else{
+                return false;
+            }
+        }else if(s[i] === '}'){
+            if(stack[stack.length-1] === '{'){
+                stack.pop();
+            }else{
+                return false;
+            }
+        }else if(s[i] === ']'){
+            if(stack[stack.length-1] === '['){
+                stack.pop();
+            }else{
+                return false;
+            }
+        }
+    }
+    return stack.length === 0 ? true: false;
+};
