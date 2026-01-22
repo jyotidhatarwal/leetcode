e84bfaaea191ad2f3f36b8fceb8e3a0dbb8b4526
Jyoti Dhatarwal <65608438+jyotidhatarwal@users.noreply.github.com>
2026-01-22 21:13:00 +0530

    Time: 0 ms (100.00%) | Memory: 53.7 MB (74.42%) - LeetSync

---
+++ 162-find-peak-element/find-peak-element.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findPeakElement = function(nums) {
+    let l = 0;
+    let r = nums.length-1;
+    while(l < r){
+        let m = l + Math.floor((r-l)/2);
+        if(nums[m] < nums[m+1]){
+            // peak exists on right side
+            l = m+1;
+        }else{
+            r = m;
+        }
+    }
+    return r;
+};
