01231e3850d118ba3e9efe7ce09294213277d25b
Jyoti Dhatarwal <65608438+jyotidhatarwal@users.noreply.github.com>
2026-01-29 08:54:31 +0530

    Time: 48 ms (16.38%) | Memory: 55.6 MB (44.43%) - LeetSync

---
+++ 1-two-sum/two-sum.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let n = nums.length;
+    for(let i=0;i<n-1;i++){
+        for(let j=i+1;j<n;j++){
+            let sum = nums[i] + nums[j];
+            if(sum === target){
+                return [i,j];
+            }
+        }
+    }
+};
