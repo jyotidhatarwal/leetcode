9f26a30d76bec29d9eec35b015fa9fd92edc2c62
Jyoti Dhatarwal <65608438+jyotidhatarwal@users.noreply.github.com>
2026-01-30 11:53:03 +0530

    Time: 0 ms (100.00%) | Memory: 53.2 MB (92.40%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.js
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.js
@@ -4,5 +4,22 @@
  * @return {number}
  */
 var strStr = function(haystack, needle) {
-    return haystack.indexOf(needle);
+    let h = haystack.length;
+    let n = needle.length;
+
+    for(let i=0;i<=h-n;i++){ // sliding window till haystack-needle length
+
+    // check if needle === window
+    let j;
+    for(j=0;j<n;j++){
+        if(haystack[i+j] !== needle[j]){
+            break;
+        }
+    }
+    if(j === n){
+        return i;
+    }
+
+    }
+     return -1;
 };
