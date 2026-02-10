b2245e85a7cdc53334343bbfbd73b05d09992fb6
Jyoti Dhatarwal <65608438+jyotidhatarwal@users.noreply.github.com>
2026-02-10 22:11:26 +0530

    Time: 0 ms (100.00%) | Memory: 55.3 MB (12.39%) - LeetSync

--- 100-same-tree/same-tree.js
+++ 100-same-tree/same-tree.js
@@ -12,11 +12,18 @@
  * @return {boolean}
  */
 var isSameTree = function(p, q) {
-    
-    function same(A,B){
-        if(!A && !B) return true; // leaf node
-        if(!A || !B) return false;
-        return (A.val === B.val && same(A.left,B.left) && same(A.right,B.right));
+    let queue = [p,q];
+    while(queue.length){
+        let p1 = queue.shift();
+        let p2 = queue.shift();
+        // leaf node
+        if(!p1 && !p2) continue;
+        if(!p1 || !p2) return false;
+        if(p1.val !== p2.val){
+            return false;
+        }
+        queue.push(p1.left,p2.left);
+        queue.push(p1.right,p2.right);
     }
-    return same(p,q);
+    return true;
 };
