b78bcee8ca5737a65110a999aa1031242b8ffcc2
Jyoti Dhatarwal <65608438+jyotidhatarwal@users.noreply.github.com>
2026-03-25 22:08:33 +0530

    Time: 7 ms (42.76%) | Memory: 56.5 MB (15.30%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,13 +4,32 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
+    // let n = nums.length;
+    // // create a map of values vs index
+    // let map = {};
+    // for(let i=0;i<n;i++){
+    //     map[nums[i]] = i;
+    // }
+
+    // // find if target - current element in array exists in map that is answer
+    // for(let i=0;i<n;i++){
+    //     let pairToFind = target - nums[i];
+    //     if(map[pairToFind] && map[pairToFind] !== i){
+    //         return [i, map[pairToFind]];
+    //     }
+    // }
+
+    // second time
     let n = nums.length;
-    for(let i=0;i<n-1;i++){
-        for(let j=i+1;j<n;j++){
-            let sum = nums[i] + nums[j];
-            if(sum === target){
-                return [i,j];
-            }
+    let map = {};
+    for(let i=0;i<n;i++){
+        map[nums[i]] = i;
+    }
+
+    for(let i=0;i<n;i++){
+        let pairToFind = target - nums[i];
+        if(map[pairToFind] && map[pairToFind] !== i){
+            return [i,map[pairToFind]];
         }
     }
 };
