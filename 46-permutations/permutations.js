12ee67efb582c62f4783ed0b62fe2e14c18757fc
Jyoti Dhatarwal <65608438+jyotidhatarwal@users.noreply.github.com>
2026-04-19 14:35:53 +0530

    Time: 0 ms (100.00%) | Memory: 60.1 MB (28.90%) - LeetSync

---
+++ 46-permutations/permutations.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permute = function(nums) {
+    let result = [];
+
+    let backtrack = (path) => {
+        if(path.length === nums.length){
+            result.push([...path]);
+            return;
+        }
+
+        for(let i=0;i<nums.length;i++){
+            if(!path.includes(nums[i])){
+                path.push(nums[i]);
+                backtrack(path);
+                path.pop();
+            }
+        }
+    }
+    backtrack([]);
+    return result;
+};
