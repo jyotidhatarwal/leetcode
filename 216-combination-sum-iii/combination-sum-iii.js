b4172b405d396d946251e4247b96cee553d4a0fc
Jyoti Dhatarwal <65608438+jyotidhatarwal@users.noreply.github.com>
2026-04-25 13:45:12 +0530

    Time: 0 ms (100.00%) | Memory: 53.3 MB (92.22%) - LeetSync

---
+++ 216-combination-sum-iii/combination-sum-iii.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number} k
+ * @param {number} n
+ * @return {number[][]}
+ */
+var combinationSum3 = function(k, n) {
+    let result = [];
+
+    let backtrack = (remainingSum,path,start) => {
+        if(path.length === k){
+            if(remainingSum === 0){
+                result.push([...path]);
+            }
+            return;
+        }
+        if(remainingSum < 0) return;
+        for(let i=start;i<=9;i++){
+            path.push(i);
+            backtrack(remainingSum-i,path,i+1);
+            path.pop();
+        }
+    }
+
+    backtrack(n,[],1);
+
+    return result;
+};
